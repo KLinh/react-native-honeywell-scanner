15190a51cb5afb52992ff82103a4ea875b23ea19
Eric-LLi <ericli1116@gmail.com>
2020-07-31 15:55:23 +1000

    update type

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,5 +4,12 @@ export declare function stopReader(): Promise<null>;
 export declare function StartScan(): () => void;
 export declare function StopScan(): () => void;
 
-export declare function on(val: 'barcodeReadSuccess' | 'barcodeReadFail', callback: () => void): void;
+export declare function on(
+	val: 'barcodeReadSuccess' | 'barcodeReadFail',
+	callback: (event: BarcodeType | null) => void
+): void;
 export declare function off(val: 'barcodeReadFail', callback: () => void): void;
+
+type BarcodeType = {
+	data: string;
+};
