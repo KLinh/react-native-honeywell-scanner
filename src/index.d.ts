a90b34a8377b2ef99a09fcbf4d91d3afcb929186
padi-dev-linhtk <linhtk@paditech.com>
2020-08-13 15:16:14 +0700

    Add compatibility check

--- src/index.d.ts
+++ src/index.d.ts
@@ -6,6 +6,7 @@ export declare function StopScan(): Promise<boolean>;
 
 export declare function on(val: 'barcodeReadSuccess' | 'barcodeReadFail', callback: (event: BarcodeType) => void): void;
 export declare function off(val: 'barcodeReadSuccess' | 'barcodeReadFail', callback: (event: BarcodeType) => void): void;
+export declare function isCompatible(val: 'isCompatible'): boolean;
 
 export type BarcodeTypes = {
 	data: string;
